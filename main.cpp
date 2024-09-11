b863edfd02e7e60dd8c702994fff4958304c7b77
Alex <Alex@ALEX-PC>
2024-09-11 23:08:30 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#define h return 0 
+
+class Message {
+public:
+    virtual void printMessage() = 0; // Чисто виртуальная функция
+};
+
+class HelloWorld : public Message {
+public:
+    void printMessage() override {
+        std::cout << "Hello world\n";
+    }
+};
+
+class DoubleHello : public Message {
+public:
+    void printMessage() override {
+        for (int i = 0; i < 2; ++i) {
+            std::cout << "Hello world\n";
+        }
+    }
+};
+
+int main(){
+    HelloWorld H;
+    DoubleHello D;
+    H.printMessage();
+    std::cout << "\n";
+    D.printMessage();
+	h;
+}
