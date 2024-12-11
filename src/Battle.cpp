a6af5bb472264458be3f41a2ded9b1b2c9e056bb
Argentime <alex_bocharov@tut.by>
2024-12-11 14:36:55 +0300

    awdaw

--- src/Battle.cpp
+++ src/Battle.cpp
@@ -3,23 +3,7 @@
 Battle::Battle(Hero& h, Enemy& e) : hero(h), enemy(e), battleFinished(false) {}
 
 void Battle::processBattle() {
-    //while (hero.getHealth() > 0 && enemy.getHealth() > 0) {
-    //    // Атака героя
-    //    hero.attack(enemy);
-    //    if (enemy.getHealth() <= 0) {
-    //        std::cout << "Вы победили врага!\n";
-    //        battleFinished = true;
-    //        return;
-    //    }
-
-    //    // Атака врага
-    //    enemy.attack(hero);
-    //    if (hero.getHealth() <= 0) {
-    //        std::cout << "Вы проиграли битву!\n";
-    //        battleFinished = true;
-    //        return;
-    //    }
-    //}
+    
 }
 
 bool Battle::isBattleFinished() const {
